4d03f17dff01f30d083cbabb2ed3bfdfbf68dc06
priscilla ma <priscillama621@gmail.com>
2022-02-12 17:52:23 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
+
+// title, description, toc, installation, usage, license, contributing, tests, questions.
+// "what is the title of your project?" input
+// inputs: description, installation instructions, usage information, contribution guidelines, test instructions = Description, Installation, Usage, Contributing, Tests
+// list: license.     badge is added near top of README, and a notice is added to the License section that explains which license.
+// input: github username. "what is your github username?" added to Questions section, with link to github.
+// input: email. added to Questions section, with instructions on how to reach me with additional questions.
+// WHEN I click on the links in the Table of Contents THEN I am taken to the corresponding section of the README.
