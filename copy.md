0d6ab710800d1cdec9c68c106e1c6dd0becea723
priscilla ma <priscillama621@gmail.com>
2022-02-13 00:10:38 -0500

    added more questions, created function to generate the markdown, created copy md file, in the function, set variables for the answer names and set up the md layout.

---
+++ copy.md
@@ -0,0 +1,46 @@
+# Fairy Butterflies
+
+![MIT license](https://img.shields.io/badge/License-MIT-yellow.svg)
+
+a really important part of puerto rican/us relations in this book goes into another thing i didn't know, which was the discovery of hookworm at the turn of the 20th century as the cause of what was considered to be a pervasive, endemic anemia afflicting the poor across the us south and puerto rico.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Credits](#credits)
+- [Contact](#contact)
+- [License](#license)
+
+## Installation
+
+at that time, the poorest workers in both regions did not wear/own shoes and struggled with unsanitary living/working conditions. this coupled with the hot, wet climates of the south and puerto rico lead to a situation where at many as 9/10 puerto rican jíbaros (peasants) had chronic hookworm disease. the us doctor who discovered this, bailey k ashford, then developed a campaign to try to eradicate this disease in puerto rico using medications as well as public health measures. the puerto rican civil government (run by white anglo-americans from the mainland) only gave ashford and his team $5000 to do this which was not even close to enough, and he spent something like a decade treating thousands of people with hookworm.
+This application is deployed with GitHub Pages [here](https://middlenamestar.github.io/portfolio).
+
+## Usage
+
+but due to underfunding and poor working conditions the whito puerto rican plantation landowners refused to improve, as well as their campaign lobbying against these public health measures and blocking the funding, ashford and the others were unable to keep treating people and those who were successfully treated just kept getting hookworm over and over and by 1930 the levels of hookworm disease were once again as bad as they'd been in 1900.
+
+## Contributing
+
+in contrast, ashford's mentor in the us south used his discovery to petition for a campaign to eradicate hookworm among the white poor of the south.
+
+## Tests
+
+his campaign received $1 million in funding from rockefeller and the support of the local state governments. this campaign eventually stretched to encompass poor black communities and by the 1970s hookworm was basically eradicated in the south. the us government declared by the 1980s that hookworm had been eradicated in the us and there was no more local transmission.
+
+## Credits
+
+at the same time, hookworm remained a problem for the working poor of puerto rico, a united states territory.
+
+## Contact
+
+Any questions and feedback, please feel free to connect with me on [github](https://github.com/middlenamestar) or email: priscillama621@gmail.com =^.^=
+
+## License
+
+Copyright (c) 2022 priscilla ma.
+
+Licensed under the MIT license.
--- index.js
+++ index.js
@@ -1,6 +1,55 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+// const gmd = require('./generateMarkdown.js');
+
+const generateMD = ({title, description, installation, repo, usage, contributing, tests, credits, github_username, email, license, fullname}) =>
+`# ${title}
+
+![MIT license](https://img.shields.io/badge/License-MIT-yellow.svg)
+
+${description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Credits](#credits)
+- [Contact](#contact)
+- [License](#license)
+
+## Installation
+
+${installation}
+This application is deployed with GitHub Pages [here](https://${github_username}.github.io/${repo}).
+
+## Usage
+
+${usage}
+
+## Contributing
+
+${contributing}
+
+## Tests
+
+${tests}
+
+## Credits
+
+${credits}
+
+## Contact
+
+Any questions and feedback, please feel free to connect with me on [github](https://github.com/${github_username}) or email: ${email} =^.^=
+
+## License
+
+Copyright (c) 2022 ${fullname}.
+
+Licensed under the ${license} license.`;
 
 // TODO: Create an array of questions for user input
 inquirer
@@ -22,6 +71,11 @@ inquirer
     },
     {
         type: 'input',
+        name: 'repo',
+        message: 'what is the name of your repo so we can link your deployment? please type the repo exactly, with any included dashes. (don\'t worry we\'ll be asking for your github username in just a few!)',
+    },
+    {
+        type: 'input',
         name: 'usage',
         message: 'describe how to use your application:',
     },
@@ -37,8 +91,13 @@ inquirer
     },
     {
         type: 'input',
+        name: 'credits',
+        message: 'shoutout! give credits here:',
+    },
+    {
+        type: 'input',
         name: 'github_username',
-        message: 'what is your github username?',
+        message: 'what is your github username, so that users can connect with you?',
     },
     {
         type: 'input',
@@ -48,31 +107,39 @@ inquirer
     {
         type: 'list',
         name: 'license',
-        message: 'please choose the license you want to use:',
-        choices: ['mit', 'meow', 'hiii'],
+        message: 'please choose the license you\'d like to use:',
+        choices: ['MIT', 'MEOW', 'HELLO', 'APACHE', 'MOZILLA'],
+    },
+    {
+        type: 'input',
+        name: 'fullname',
+        message: 'please enter your full name as part of the license, spaces included, capitalization and all (if you\'d like).',
     },
   ])
   .then((answers) =>{
-    //   console.log(answers);
-    fs.writeFile('README.md', JSON.stringify(answers, null, '\t'), 'utf8', (err) =>
+    // console.log();
+    const markdown = generateMD(answers);
+    fs.writeFile('copy.md', markdown, 'utf8', (err) =>
         err ? console.log(err) : console.log('success :)')
     );
   });
 
+// JSON.stringify(answers, null, '\t')
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {}
 
 // Function call to initialize app
-init();
+// init();
 
 // TITLE, DESCRIPTION, TABLE OF CONTENTS, INSTALLATION, USAGE, CONTRIBUTING, TESTS, QUESTIONS, LICENSE.
 
-// list: license.💗     badge is added near top of README, and a notice is added to the License section that explains which license.
+// list: license.     badge is added near top of README, and a notice is added to the License section that explains which license.
 
-// input: github username. "what is your github username?"💗 added to Questions section, with link to github.
+// input: github username. "what is your github username?" added to Questions section, with link to github.
 
-// input: email.💗 added to Questions section, with instructions on how to reach me with additional questions.
+// input: email. added to Questions section, with instructions on how to reach me with additional questions.
 // WHEN I click on the links in the Table of Contents THEN I am taken to the corresponding section of the README.
